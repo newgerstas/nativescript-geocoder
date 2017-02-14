5a56213b74fe9513e4d1d60963827baa77b668be
Stanislav_Gerasimov <Stanislav_Gerasimov@epam.com>
2017-02-14 21:24:30 +0300

    Fixed java array convertion to js

--- src/index.android.ts
+++ src/index.android.ts
@@ -1,18 +1,49 @@
-import { ILocation, IAddress, IRegion, Geocoder as GeocoderBase } from ".";
+declare var android;
 import * as utils from "utils/utils";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
+
+export interface ILocation {
+    latitude: number;
+    longitude: number;
+}
+
+export interface IRegion {
+    southwest: ILocation;
+    nowtheast: ILocation;
+}
+
+export interface IAddress {
+    latitude: number;
+    longitude: number;
+    addressLine: string;
+    countryName: string;
+    countryCode: string;
+    locality: string
+    featureName: string;
+    phone: string;
+    postalCode: string;
+    administrativeArea: string;
+}
+
+export interface IGeocoder {
+    getByLocation(location: ILocation): Observable<Array<IAddress>>;
+    getByName(name: string, region?: IRegion): Observable<Array<IAddress>>;
+}
+
 
 class Address implements IAddress {
 
     constructor(private android: any) {
     }
 
-    get location(): ILocation {
-        return {
-            latitude: this.android.getLatitude(),
-            longitude: this.android.getLongitude()
-        };
+    get latitude(): number {
+        return this.android.getLatitude();
+    }
+
+    get longitude(): number {
+        return this.android.getLongitude();
     }
 
     get addressLine(): string {
@@ -48,7 +79,7 @@ class Address implements IAddress {
     }
 }
 
-export class Geocoder implements GeocoderBase {
+export class Geocoder implements IGeocoder {
 
     public static readonly MAX_RESULTS: number = 10;
 
@@ -63,12 +94,12 @@ export class Geocoder implements GeocoderBase {
         return Observable.create(observer => {
             try {
                 let addresses = this.android.getFromLocation(location.latitude, location.longitude, Geocoder.MAX_RESULTS);
-                observer.next(addresses || []);
+                observer.next(addresses);
                 observer.complete();
             } catch (error) {
                 observer.error(error);
             }
-        }).map(ad => new Address(ad));
+        }).map(ads => this.javaToJsArray(ads, ad => new Address(ad)));
     }
 
     getByName(name: string, region?: IRegion): Observable<Array<IAddress>> {
@@ -81,11 +112,20 @@ export class Geocoder implements GeocoderBase {
                 } else {
                     addresses = this.android.getFromLocationName(name, Geocoder.MAX_RESULTS);
                 }
-                observer.next(addresses || []);
+                observer.next(addresses);
                 observer.complete();
             } catch (error) {
                 observer.error(error);
             }
-        }).map(ad => new Address(ad));
+        }).map(ads => this.javaToJsArray(ads, ad => new Address(ad)));
+    }
+
+    //TODO: Figure out the better way to convert java ArrayList to JS array
+    private javaToJsArray<T>(array: any, convert: Function) {
+        let output = [], count = array.size();
+        for (var i = 0; i < count; i++) {
+            output.push(convert(array.get(i)));
+        }
+        return output;
     }
 }
